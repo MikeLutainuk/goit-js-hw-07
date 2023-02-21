f3f4c57362f95ff00281f8073be579ca82ee5de6
MikeLutainuk <113596204+MikeLutainuk@users.noreply.github.com>
2023-02-21 20:37:26 +0200

    up

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+function createPhotoGallery(items) {
+    return items.map(item =>
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join('');
+};
+
+const divPhotos = document.querySelector('.gallery');
+const addPhotoGallery = createPhotoGallery(galleryItems);
+
+divPhotos.insertAdjacentHTML("afterbegin", addPhotoGallery);
+
+divPhotos.addEventListener('click', onPhotoClick); 
+
+function onPhotoClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+  
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">`);
+  instance.show();
+  
+    divPhotos.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+} 
+
+
